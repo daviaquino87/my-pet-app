0f6a921e50b6d0d35c652b2161c53dc68ad632aa
Alex Alan Nunes <aalannunesdelima@apple.com>
2023-05-04 21:11:04 -0300

    chore: alterar projeto par typescript e FC

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Center, Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Center h={"100vh"}>
+          <Heading>Hello</Heading>
+        </Center>
+      </header>
+    </div>
+  );
+}
+
+export default App;
