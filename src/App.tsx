ba1eae8c572aefd26427b8fb259679262c2405d5
Alex Alan Nunes <aalannunesdelima@apple.com>
2023-05-05 10:30:39 -0300

    chore: add react router dom e configurar as rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { Link, Outlet, Route, Routes } from 'react-router-dom';
 import { AuthPage } from './components/auth';
+import { Card, CardBody } from '@chakra-ui/react';
+
+function InitialPage() {
+  return (
+    <Card>
+      <CardBody>
+        <Link to="/login">Login</Link>
+        &nbsp;
+        <Link to="/register">Registrar</Link>
+      </CardBody>
+      <Outlet />
+    </Card>
+  );
+}
 
 export default function App() {
-  return <AuthPage type="register" />;
+  return (
+    <Routes>
+      {/* convert to single componente */}
+
+      <Route path="/" element={<InitialPage />}>
+        <Route path="/login" element={<AuthPage type="login" />} />
+        <Route path="/register" element={<AuthPage type="register" />} />
+      </Route>
+    </Routes>
+  );
 }
