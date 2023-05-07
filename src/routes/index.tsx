b28c67805f6bccdb95fbb8c74927606e2f9fce58
Alex Alan Nunes <aalannunesdelima@apple.com>
2023-05-07 00:31:16 -0300

    refactor: implement routes with right routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
-import { AuthPage } from '../components/auth';
+import { LoginPage } from '../pages/auth/login';
+import { RegisterPage } from '../pages/auth/register';
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<h3>hi</h3>} />
-      <Route path="/login" element={<AuthPage type="login" />} />
-      <Route path="/register" element={<AuthPage type="register" />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
 }
