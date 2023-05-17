a2d600cbc4202a0a24de7d88948f0bb169cc8c8e
Alex Alan Nunes <aalannunesdelima@apple.com>
2023-05-17 13:44:47 -0300

    feat: interceptors and axios instance

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,33 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://mypet.codewizard.com.br',
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
+
+export const privateApi = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
+
+privateApi.interceptors.request.use((config) => {
+  const token = localStorage.getItem('@data');
+  if (token) {
+    const data = JSON.parse(token);
+    config.headers.Authorization = `Baerer ${data.token}`;
+  }
+  return config;
+});
+
+privateApi.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  function (error) {
+    console.log(error.response.status);
+    if (error.response.status === 401) {
+      window.location.replace('/login');
+    }
+    return Promise.reject(error);
+  }
+);
 
 export { api };
