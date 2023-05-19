175d015c7dd53646d1c90b057d4be3cec78358ff
Alex Alan Nunes <aalannunesdelima@apple.com>
2023-05-19 18:08:15 -0300

    feat: add currency input

--- src/pages/new/index.tsx
+++ src/pages/new/index.tsx
@@ -6,28 +6,65 @@ import {
   IconButton,
   Stack,
   Input,
+  Button,
 } from '@chakra-ui/react';
-import { ChangeEvent } from 'react';
+import { Ref, forwardRef } from 'react';
 import { CurrencyInput } from 'react-currency-mask';
+import { Controller, useForm } from 'react-hook-form';
+interface CustomCurrencyInputProps {
+  onChange: (value: number) => void;
+}
+
+const CustomCurrencyInput = forwardRef(
+  (
+    { onChange, ...props }: CustomCurrencyInputProps,
+    ref: Ref<HTMLInputElement>
+  ) => {
+    return (
+      <CurrencyInput
+        {...props}
+        ref={ref}
+        defaultValue={0}
+        onChangeValue={(originalValue: any) => {
+          onChange(originalValue);
+        }}
+        InputElement={<Input placeholder="R$ Valor" />}
+        currency="BRL"
+      />
+    );
+  }
+);
+
 export function NewSpendingPage() {
+  const { control, handleSubmit } = useForm({
+    mode: 'onBlur',
+    defaultValues: {
+      price: 0,
+    },
+  });
+
+  const submit = (values: any) => {
+    console.log('debug:name', values);
+  };
   return (
     <Container maxW="96" pt={20}>
       <Stack spacing={10}>
-        <HStack>
+        <HStack spacing="5">
           <IconButton icon={<ArrowBackIcon />} aria-label="Voltar" />
           <Heading size="md">Nova despesa</Heading>
         </HStack>
 
-        <CurrencyInput
-          onChangeValue={(
-            event: ChangeEvent,
-            originalValue: any,
-            maskedValue: any
-          ) => {
-            console.log(event, originalValue, maskedValue);
-          }}
-          InputElement={<Input autoFocus placeholder="Valor" />}
-        />
+        <Stack as="form" spacing="10" onSubmit={handleSubmit(submit)}>
+          <Controller
+            control={control}
+            name="price"
+            render={({ field }) => <CustomCurrencyInput {...field} />}
+          />
+
+          <Button type="submit" colorScheme="green">
+            Salvar
+          </Button>
+        </Stack>
       </Stack>
     </Container>
   );
