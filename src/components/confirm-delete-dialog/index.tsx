d482ff72b84a9a7a011f2d2a1c9a0e7e0dcfee78
Alex Alan Nunes <aalannunesdelima@apple.com>
2023-05-24 09:49:06 -0300

    feat: add confirm delete dialog

---
+++ src/components/confirm-delete-dialog/index.tsx
@@ -0,0 +1,59 @@
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+} from '@chakra-ui/react';
+import { useRef } from 'react';
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+  onOK: () => void;
+  title: string;
+  description: string;
+  cancelText?: string;
+  okText?: string;
+}
+
+export function ConfirmDeleteDialog({
+  isOpen,
+  onClose,
+  onOK,
+  title,
+  description,
+  cancelText = 'Cancelar',
+  okText = 'Remover',
+}: Props) {
+  const cancelRef = useRef(null);
+
+  return (
+    <AlertDialog
+      isOpen={isOpen}
+      leastDestructiveRef={cancelRef}
+      onClose={onClose}
+    >
+      <AlertDialogOverlay>
+        <AlertDialogContent>
+          <AlertDialogHeader fontSize="lg" fontWeight="bold">
+            {title}
+          </AlertDialogHeader>
+
+          <AlertDialogBody>{description}</AlertDialogBody>
+
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onClose}>
+              {cancelText}
+            </Button>
+            <Button colorScheme="red" onClick={onOK} ml={3}>
+              {okText}
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  );
+}
