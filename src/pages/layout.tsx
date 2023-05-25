88c0a09633cad224167ae207d74899164f25d4c1
Alex Alan Nunes <aalannunesdelima@apple.com>
2023-05-25 09:51:25 -0300

    refactor: implement composition

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,11 +1,26 @@
 import { Flex } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 import { HeaderApp } from '../components/header';
+import { Logo } from '../components/logo/logo';
+import { Username } from '../components/username/username';
+import { ToggleTheme } from '../components/toggle-theme/toggle-theme';
+import { ReportsLink } from '../components/reports-link/reports-link';
+import { Logout } from '../components/logout/logout';
 
 export function LayoutPage() {
   return (
     <Flex direction="column" minH="100vh">
-      <HeaderApp />
+      <HeaderApp
+        logo={<Logo />}
+        user={<Username />}
+        actions={
+          <>
+            <ToggleTheme />
+            <ReportsLink />
+            <Logout />
+          </>
+        }
+      />
       <Flex flex={1} direction="column">
         <Outlet />
       </Flex>
