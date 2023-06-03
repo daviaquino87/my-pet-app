98785be736a4ed00d948dd4b892441e4e110d121
Alex Alan Nunes <aalannunesdelima@apple.com>
2023-06-03 20:57:52 -0300

    use-case: test prod

--- src/components/input-currency/index.tsx
+++ src/components/input-currency/index.tsx
@@ -17,6 +17,8 @@ export const CustomCurrencyInput = forwardRef(
 
     const inputSize: InputProps['size'] = isMobile ? 'lg' : 'md';
 
+    console.log('CustomCurrencyInput', { value });
+
     return (
       <CurrencyInput
         {...props}
